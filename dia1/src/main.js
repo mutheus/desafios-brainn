d7313fb4735565d046145adf70d837cc88716edf
Matheus Almeida <matheusalmeida016@gmail.com>
2021-08-19 06:44:37 -0400

    Exercício 04

--- dia1/src/main.js
+++ dia1/src/main.js
@@ -1,6 +1,13 @@
 import './style.css';
 
-document.querySelector('[data-js="app"]').innerHTML = `
+document.querySelector('[data-js="app"]');
+app.innerHTML = `
   <h1>B. Academy</h1>
   <p>Boas vindas à semana de pré-work para o Bootcamp em React.js 😁</p>
 `;
+
+app.insertAdjacentHTML('beforebegin', '<button data-js="btn">Click me</button>');
+
+const btn = document.querySelector('[data-js="btn"]');
+
+btn.addEventListener('click', () => app.style.display === 'none' ? app.style.display = 'block' : app.style.display = 'none');
